70b9da586bc748786a2ef72cc34d2014a319cb91
Mohamed ElTaweel <mohamedmagdy.9080@gmail.com>
2021-01-01 20:24:04 +0200

    Currency Convert View Model

---
+++ SWENSON-HE-TASK/Modules/CurrencyConvert/ViewModel/CurrencyConvertViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  CurrencyConvertViewModel.swift
+//  SWENSON-HE-TASK
+//
+//  Created by Mohamed El-Taweel on 1/1/21.
+//  Copyright © 2021 SWENSON-HE. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol CurrencyConvertViewModeling {
+    var localCurrencyName: Observable<String>{get}
+    var localCurrencyValue: BehaviorSubject<String>{get}
+    var foriegnCurrencyName: Observable<String>{get}
+    var foriegnCurrencyValue: BehaviorSubject<String>{get}
+    
+    func getInitialRatio()
+}
+
+class CurrencyConvertViewModel: CurrencyConvertViewModeling{
+    var localCurrencyName: Observable<String>{ localCurrencyNameSubject.asObservable() }
+    var localCurrencyValue: BehaviorSubject<String>
+    var foriegnCurrencyName: Observable<String>{ foreignCurrencyNameSubject.asObservable() }
+    var foriegnCurrencyValue: BehaviorSubject<String>
+    private var localCurrencyNameSubject: PublishSubject<String>
+    private var foreignCurrencyNameSubject: PublishSubject<String>
+    private var foreignCurrency: String
+    private var currencyConverter: CurrencyConverting
+    private var disposeBag = DisposeBag()
+    
+    init(foreignCurrency: String,currencyConverter: CurrencyConverting) {
+        self.foreignCurrency = foreignCurrency
+        self.currencyConverter = currencyConverter
+        self.localCurrencyValue = BehaviorSubject(value: "0.0")
+        self.foriegnCurrencyValue = BehaviorSubject(value: "0.0")
+        self.foreignCurrencyNameSubject = PublishSubject()
+        self.localCurrencyNameSubject = PublishSubject()
+        observeLocalCurrencyValue()
+    }
+    
+    func getInitialRatio() {
+        foreignCurrencyNameSubject.onNext(foreignCurrency)
+        localCurrencyNameSubject.onNext(currencyConverter.baseCurrency)
+        localCurrencyValue.onNext("1.0")
+        let foreginCurrencyValue = currencyConverter.convertCurrency(localCurrencyAmount: 1.0, foreginCurrency: foreignCurrency)
+        self.foriegnCurrencyValue.onNext("\(foreginCurrencyValue)")
+    }
+    
+    private func observeLocalCurrencyValue(){
+        self
+            .localCurrencyValue
+            .map{ Double($0) ?? 0.0 }
+            .subscribe {[weak self] (newValue) in
+                let foreginCurrencyValue = self?.currencyConverter.convertCurrency(localCurrencyAmount: 1.0, foreginCurrency: self?.foreignCurrency ?? "USD")
+                self?.foriegnCurrencyValue.onNext("\(foreginCurrencyValue ?? 0.0)")
+                
+        }.disposed(by: disposeBag)
+    }
+    
+}
