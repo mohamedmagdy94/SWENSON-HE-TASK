b88bd8b78cab44ca5003d55c1270c89aab05c983
Mohamed ElTaweel <mohamedmagdy.9080@gmail.com>
2021-01-01 14:55:40 +0200

    DI Protocol

---
+++ SWENSON-HE-TASK/Modules/CurrencySelect/DI/CurrencySelectDependencyInjector.swift
@@ -0,0 +1,14 @@
+//
+//  CurrencySelectDependencyInjector.swift
+//  SWENSON-HE-TASK
+//
+//  Created by Mohamed El-Taweel on 1/1/21.
+//  Copyright © 2021 SWENSON-HE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol CurrencySelectDependencyInjecting {
+    func resolve()->UIViewController
+}
